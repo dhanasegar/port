c0fe18f20d16feb380f867ecc78f7716a266fa75
dhanasegar <dhanasegarbit@gmail.com>
2025-01-11 01:03:14 +0530

    avatar urls

--- main/models.py
+++ main/models.py
@@ -163,12 +163,10 @@
 #     file = models.BinaryField()  # To store the file in binary format
 #     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
-import base64
 
 
 class Skill(models.Model):
@@ -190,26 +188,15 @@ class UserProfile(models.Model):
         verbose_name = 'User Profile'
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar= models.BinaryField(blank=True, null=True)
+    avatar = models.URLField(max_length=200, blank=True, null=True)  # Avatar field as URL
     title = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     skills = models.ManyToManyField(Skill, blank=True)
     cv = models.FileField(upload_to='resumes/', null=True, blank=True)
     cv_binary = models.BinaryField(null=True, blank=True)
 
-    def save(self, *args, **kwargs):
-        if self.cv and not self.cv_binary:
-            self.cv_binary = self.cv.read()
-            self.cv.close()
-        super(UserProfile, self).save(*args, **kwargs)
-
-    def set_avatar(self, file_data):
-        self.avatar = file_data.read()
-
-    def get_avatar_base64(self):
-        if self.avatar:
-            return base64.b64encode(self.avatar).decode('utf-8')
-        return None
+    def __str__(self):
+        return self.user.username
 
 
 class ContactProfile(models.Model):
@@ -233,19 +220,11 @@ class Testimonial(models.Model):
         verbose_name = 'Testimonial'
         ordering = ["id"]
 
-    thumbnail_binary = models.BinaryField(blank=True, null=True)
+    thumbnail = models.URLField(blank=True, null=True)  # Thumbnail field as URL
     name = models.CharField(max_length=200, blank=True, null=True)
     role = models.CharField(max_length=200, blank=True, null=True)
     quote = models.CharField(max_length=500, blank=True, null=True)
     is_active = models.BooleanField(default=True)
-
-    def set_thumbnail(self, file_data):
-        self.thumbnail_binary = file_data.read()
-
-    def get_thumbnail_base64(self):
-        if self.thumbnail_binary:
-            return base64.b64encode(self.thumbnail_binary).decode('utf-8')
-        return None
 
     def __str__(self):
         return self.name
@@ -257,7 +236,7 @@ class Media(models.Model):
         verbose_name = 'Media'
         ordering = ["name"]
 
-    image_binary = models.BinaryField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)  # Image field as URL
     url = models.URLField(blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     is_image = models.BooleanField(default=True)
@@ -266,14 +245,6 @@ class Media(models.Model):
         if self.url:
             self.is_image = False
         super(Media, self).save(*args, **kwargs)
-
-    def set_image(self, file_data):
-        self.image_binary = file_data.read()
-
-    def get_image_base64(self):
-        if self.image_binary:
-            return base64.b64encode(self.image_binary).decode('utf-8')
-        return None
 
     def __str__(self):
         return self.name
@@ -289,26 +260,14 @@ class Portfolio(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
     body = RichTextField(blank=True, null=True)
-    image = models.ImageField(blank=True, null=True, upload_to="portfolio")
-    image_binary = models.BinaryField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)  # Portfolio image as URL
     slug = models.SlugField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.name)
-        if self.image and not self.image_binary:
-            with self.image.open("rb") as img_file:
-                self.image_binary = img_file.read()
         super(Portfolio, self).save(*args, **kwargs)
-
-    def set_image_binary(self, file_data):
-        self.image_binary = file_data.read()
-
-    def get_image_base64(self):
-        if self.image_binary:
-            return base64.b64encode(self.image_binary).decode('utf-8')
-        return None
 
     def __str__(self):
         return self.name
@@ -328,26 +287,14 @@ class Blog(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
     body = RichTextField(blank=True, null=True)
-    image = models.ImageField(blank=True, null=True, upload_to="blog")
-    image_binary = models.BinaryField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)  # Blog image as URL
     slug = models.SlugField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.name)
-        if self.image and not self.image_binary:
-            with self.image.open("rb") as img_file:
-                self.image_binary = img_file.read()
         super(Blog, self).save(*args, **kwargs)
-
-    def set_image_binary(self, file_data):
-        self.image_binary = file_data.read()
-
-    def get_image_base64(self):
-        if self.image_binary:
-            return base64.b64encode(self.image_binary).decode('utf-8')
-        return None
 
     def __str__(self):
         return self.name
@@ -375,3 +322,4 @@ class Resume(models.Model):
     name = models.CharField(max_length=255)
     file = models.BinaryField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
