c6fa7a3f5ca1ea407344cc5c5a7c333634b21bf7
dhanasegar <dhanasegarbit@gmail.com>
2025-01-18 21:30:53 +0530

    not found error 7

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -131,7 +131,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "TRUE").lower() == "FALSE"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS",'*',).split(" ")
 
 # Application definition
 INSTALLED_APPS = [
