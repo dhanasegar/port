e44df0dcb0b95c8d4bd58464b8825eadfc247dfb
dhanasegar <dhanasegarbit@gmail.com>
2025-01-21 00:17:52 +0530

    commit 7

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 LOGIN_REDIRECT_URL = "/" 
 
-LOGIN_URL = "/blog"
+LOGIN_URL = "/index"
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
